55c3c016c4a6c9f0c608454223706a8e9d70bdbc
Kitkathawk <susana.vale.lopes@gmail.com>
2020-10-31 16:49:22 +0000

    Jasmine template

---
+++ spec/AppTest.js
@@ -0,0 +1,7 @@
+describe("Sum", function() {
+    describe("when function runs", function() {
+        it("should add two numbers", function() {
+            expect(sum(2,2)).toBe(4);
+        });
+    });
+});
